ede2f9bcc8942b5319355785ebfe92f3be3c7640
Harsh Verma <hv0626266@gmail.com>
2026-01-08 17:24:41 +0530

    Implement user authentication and layout structure with Redux integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-
+import { useEffect, useState } from 'react'
+import {Outlet, useDispatch} from 'react-router-dom'
+import authService from './appwrite/auth';
+import {login, logout } from './Store/authslice';
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading,setloading] = useState(true);
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authService.currentuser.then((user)=>{
+      if(user){
+        dispatch(login({user}))
+      }
+      else{
+        dispatch(logout());
+      }
+    }).finally(()=> setloading(false))
+  },[])
+
+
+
 
   return (
-    <>
-    <div className='text-center bg-gray-500 p-5 text-white'>going to make a application  and learn react</div>
-    </>
+    !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            <Outlet/>
+          </main>
+          <Footer/>
+        </div>
+      </div>
+    ) : <div>loading.....</div>
   )
 }
 
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,5 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit';
+import {authSlice} from '../Store/authslice'
 
 export const Store = configureStore({
-    
+    reducer:{
+        auth:authSlice,
+    }
 })
